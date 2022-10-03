35035536519276109cbb7e9900c1a43c6394a88d
BatterB <dimkahey@gmail.com>
2022-10-03 17:52:59 +0300

    Class AmericanSiSystem.kt was implemented

--- src/main/java/AmericanSiSystem.kt
+++ src/main/java/AmericanSiSystem.kt
@@ -1,18 +1,18 @@
 class AmericanSiSystem {
     companion object{
         fun meterToYard(meters: Float): Float{
-            return 0.0F
+            return meters * 1.09F
         }
 
         fun yardToMeter(yards: Float): Float{
-            return 0.0F
+            return yards * 0.9144F
         }
 
         fun poundToKilogram(pounds: Float): Float {
-            return 0.0F
+            return pounds * 0.454F
         }
         fun kilogramToPound(kilos: Float) : Float{
-            return 0.0F
+            return kilos * 2.2F
         }
     }
 }
--- src/test/java/AmericanSiSystemTest.kt
+++ src/test/java/AmericanSiSystemTest.kt
@@ -4,29 +4,29 @@ import kotlin.test.assertEquals
 class AmericanSiSystemTest {
     @Test
     fun shouldTransformToMeters(){
-        val actual = AmericanSiSystem.yardToMeter(5.468F)
-        val expected = 5.0F
+        val actual = AmericanSiSystem.yardToMeter(5.0F)
+        val expected = 4.572F
         assertEquals(expected,actual)
     }
 
     @Test
     fun shouldTransformToYard(){
         val actual = AmericanSiSystem.meterToYard(4.572F)
-        val expected = 5.0F
+        val expected = 4.98348F
         assertEquals(expected,actual)
     }
 
     @Test
     fun shouldTransformToKilo(){
-        val actual = AmericanSiSystem.poundToKilogram(11.023F)
-        val expected = 5F
+        val actual = AmericanSiSystem.poundToKilogram(11F)
+        val expected = 4.994F
         assertEquals(expected,actual)
     }
 
     @Test
     fun shouldTransformToPounds(){
         val actual = AmericanSiSystem.kilogramToPound(5F)
-        val expected = 11.023F
+        val expected = 11F
         assertEquals(expected,actual)
     }
 
