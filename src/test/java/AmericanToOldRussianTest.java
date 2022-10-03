b02e7d1da1ddfe4f3ee21985a30851139260aec8
Denis <miner8282@mail.ru>
2022-10-03 17:57:19 +0300

    Небольшой фикс тестиков

--- src/test/java/AmericanToOldRussianTest.java
+++ src/test/java/AmericanToOldRussianTest.java
@@ -6,21 +6,21 @@ class AmericanToOldRussianTest {
 
     @Test
     void yardToArshin() {
-        assertEquals(1.0, AmericanToOldRussian.yardToArshin(2.0));
+        assertEquals(2.5714286, AmericanToOldRussian.yardToArshin(2.0));
     }
 
     @Test
     void arshinToYard() {
-        assertEquals(1.0, AmericanToOldRussian.arshinToYard(2.0));
+        assertEquals(1.555555538, AmericanToOldRussian.arshinToYard(2.0));
     }
 
     @Test
     void russianPoundToAmericanPound() {
-        assertEquals(1.0, AmericanToOldRussian.russianPoundToAmericanPound(2.0));
+        assertEquals(1.805639028, AmericanToOldRussian.russianPoundToAmericanPound(2.0));
     }
 
     @Test
     void americanPoundToRussianPound() {
-        assertEquals(1.0, AmericanToOldRussian.americanPoundToRussianPound(2.0));
+        assertEquals(2.2152822, AmericanToOldRussian.americanPoundToRussianPound(2.0));
     }
 }
