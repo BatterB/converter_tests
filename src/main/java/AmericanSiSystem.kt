358abcc55cb6462cc1db96e3fb297034c829429d
BatterB <dimkahey@gmail.com>
2022-10-03 17:32:25 +0300

    Added empty fun to AmericanSiSystem

---
+++ src/main/java/AmericanSiSystem.kt
@@ -0,0 +1,18 @@
+class AmericanSiSystem {
+    companion object{
+        fun meterToYard(meters: Float): Float{
+            return 0.0F
+        }
+
+        fun yardToMeter(yards: Float): Float{
+            return 0.0F
+        }
+
+        fun poundToKilogram(pounds: Float): Float {
+            return 0.0F
+        }
+        fun kilogramToPound(kilos: Float) : Float{
+            return 0.0F
+        }
+    }
+}
