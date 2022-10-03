30c9696ce2e074f141767a287debc94286ee1ada
BatterB <dimkahey@gmail.com>
2022-10-03 16:25:48 +0300

    added kotlin support

---
+++ src/main/java/AmericanSystem.kt
@@ -0,0 +1,5 @@
+class AmericanSystem {
+    fun hello(){
+        println("hihi")
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        AmericanSystem boo = new AmericanSystem();
+        boo.hello();
+    }
+
 }
